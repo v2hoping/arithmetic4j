72bfe4943b3d80c7d08f43bb9df3a5ac348b1b68
gwdgitnom <gwd163nom@163.com>
2016-08-23 14:09:37 +0800

    init

---
+++ src/main/java/StatisticalLearning/DataMining_SVM/libsvm/svm_node.java
@@ -0,0 +1,14 @@
+package StatisticalLearning.DataMining_SVM.libsvm;
+/**
+ * 
+ * svm�����ڵ�
+ * @author lyq
+ *
+ */
+public class svm_node implements java.io.Serializable
+{
+	//�ڵ�����
+	public int index;
+	//�ڵ��ֵ
+	public double value;
+}
